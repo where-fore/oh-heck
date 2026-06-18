1f21d27b2e00592bfaf214f1808414735aa83454
wherefore <wherefore0@gmail.com>
2026-06-18 17:32:39 -0400

    initial setup

---
+++ project/sprite_2d.gd
@@ -0,0 +1,7 @@
+extends Sprite2D
+
+var speed:float = 60
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotation_degrees += delta*speed
