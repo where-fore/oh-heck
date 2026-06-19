e242d3e7d70b536fee234142c942940192eee5e7
wherefore <wherefore0@gmail.com>
2026-06-19 13:33:26 -0400

    card iconography

--- project/globals/names.gd
+++ project/globals/names.gd
@@ -1,13 +1,13 @@
 extends Node
 
-const suit_demon:StringName = &"Demons"
+const suit_devil:StringName = &"devils"
 const suit_angel:StringName = &"Angels"
 const suit_sun:StringName = &"Suns"
 const suit_moon:StringName = &"Moons"
-const suits:Array[StringName] = [suit_demon, suit_angel, suit_sun, suit_moon]
+const suits:Array[StringName] = [suit_devil, suit_angel, suit_sun, suit_moon]
 const suit_unassigned:StringName = &"UNITIALIZED SUIT"
 
-const demon_colour:Color = Color.PURPLE
-const angel_colour:Color = Color.LIGHT_YELLOW
-const sun_colour:Color = Color.DARK_ORANGE
-const moon_colour:Color = Color.DARK_BLUE
+const devil_colour:Color = Color("B20D30")
+const angel_colour:Color = Color("DBCBD8")
+const sun_colour:Color = Color("F7B53B")
+const moon_colour:Color = Color("564787")
--- project/systems/ui of game logic/ui_card.gd
+++ project/systems/ui of game logic/ui_card.gd
@@ -4,22 +4,38 @@ class_name UICard
 var card:Card
 @export var colour_swatch:ColorRect
 @export var value_label:Label
+@export var angel_icon:TextureRect
+@export var devil_icon:TextureRect
+@export var moon_icon:TextureRect
+@export var sun_icon:TextureRect
+@onready var icons:Array[TextureRect] = [angel_icon, devil_icon, moon_icon, sun_icon]
 signal ui_card_selected(ui_card:UICard)
 
 func setup(card_to_assign:Card) -> void:
+	
 	card = card_to_assign
+	
+	disable_all_icons()
+	
 	match card.suit:
-		Names.suit_demon:
-			colour_swatch.color = Names.demon_colour
+		Names.suit_devil:
+			colour_swatch.color = Names.devil_colour
+			devil_icon.visible = true
 		Names.suit_angel:
 			colour_swatch.color = Names.angel_colour
+			angel_icon.visible = true
 		Names.suit_sun:
 			colour_swatch.color = Names.sun_colour
+			sun_icon.visible = true
 		Names.suit_moon:
 			colour_swatch.color = Names.moon_colour
+			moon_icon.visible = true
 	
 	value_label.text = str(card.value)
 
+func disable_all_icons() -> void:
+	for icon:TextureRect in icons:
+		icon.visible = false
 
 func _on_button_pressed() -> void:
 	ui_card_selected.emit(self)
