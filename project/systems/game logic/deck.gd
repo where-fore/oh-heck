0ffd59ed905c9445a2b78176313c165dde0092ef
wherefore <wherefore0@gmail.com>
2026-06-20 08:52:24 -0400

    functionality: can draw specific card
    if i wanted (i will for the tutorial)

--- project/systems/game logic/deck.gd
+++ project/systems/game logic/deck.gd
@@ -27,3 +27,20 @@ func draw_top_card() -> Card:
 	if not cards_in_deck: push_error("deck was empty")
 	var card_to_draw:Card = cards_in_deck.pop_back()
 	return card_to_draw
+
+func draw_specific_card(value:int, suit:StringName) -> Card:
+	if value <= 0 or value > Rules.max_card_value_to_create:
+		push_error("told to draw specific card of value: ", value, ", but that's illegal")
+	if suit not in Names.suits:
+		push_error("told to draw specific card of suit: ", suit, ", but that's illegal")
+	
+	var card_drawn:Card
+	for card:Card in cards_in_deck:
+		if card.suit == suit:
+			if card.value == value:
+				card_drawn = card
+	if not card_drawn: push_error("never found card in deck: ", value, " of ", suit)
+	
+	cards_in_deck.erase(card_drawn)
+	
+	return card_drawn
