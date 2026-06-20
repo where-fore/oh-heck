02e8a540a27e37ea61da3377c51ec03dcaf10ad5
wherefore <wherefore0@gmail.com>
2026-06-20 11:19:07 -0400

    dialogue bubble tweaks

---
+++ project/globals/dialogue.gd
@@ -0,0 +1,5 @@
+extends Node
+
+@warning_ignore_start("unused_signal")
+signal new_dialogue_bark(text:String)
+@warning_ignore_restore("unused_signal")
--- project/systems/main game/dialogue_popup.gd
+++ project/systems/main game/dialogue_popup.gd
@@ -2,31 +2,52 @@ extends TextureRect
 
 @export var my_label:RichTextLabel
 var typewriter_characters_per_second:float = 12
+var scroll_text_tween:Tween
+var fade_out_bubble_tween:Tween
 
 func _ready() -> void:
 	visible = false
 	my_label.text = ""
+	Dialogue.new_dialogue_bark.connect(scroll_text)
+	
 	scroll_text("HAHAHA HAHAHAHAHA")
+	#await get_tree().create_timer(3).timeout
+	#scroll_text("HEHEH HAHAHAHAHA")
 
 func scroll_text(new_text:String) -> void:
-	if not visible: await tween_bubble_in()
-		
 	my_label.visible_characters = 0
 	my_label.text = new_text
+	
+	await tween_bubble_in()
 	
 	var total_characters:int = my_label.get_total_character_count()
 	var duration:float = total_characters / typewriter_characters_per_second
 	
-	var visible_characters_tween:Tween = create_tween()
-	visible_characters_tween.tween_property(my_label, "visible_characters", total_characters, duration)
+	scroll_text_tween = create_tween()
+	scroll_text_tween.tween_property(my_label, "visible_characters", total_characters, duration)
+	
+	await scroll_text_tween.finished
+	await get_tree().create_timer(1 + total_characters/8.0).timeout
+	tween_bubble_out()
 
 func tween_bubble_in() -> bool:
 	scale = Vector2(0,0)
 	visible = true
+	modulate = Color(1,1,1,1)
 	var tween:Tween = create_tween()
 	tween.set_trans(Tween.TRANS_ELASTIC)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "scale", Vector2(1,1), 1)
 	
 	await tween.finished
+	return true
+
+func tween_bubble_out() -> bool:
+	var my_color:Color = modulate
+	fade_out_bubble_tween = create_tween()
+	fade_out_bubble_tween.set_ease(Tween.EASE_IN)
+	fade_out_bubble_tween.set_trans(Tween.TRANS_QUAD)
+	fade_out_bubble_tween.tween_property(self, "modulate", Color(my_color.r, my_color.g, my_color.b, 0), 1.5)
+	
+	await fade_out_bubble_tween.finished
 	return true
